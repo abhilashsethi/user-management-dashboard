bd6d9591bb31165ee1df56c6e98b6f72dd38d627
abhilash <abhilashsethi9@gmail.com>
2026-06-27 13:05:37 +0530

    chore: initialize React project with Vite and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-400">
+        User Management Dashboard
+      </h1>
+    </div>
+  );
+}
+
+export default App;
